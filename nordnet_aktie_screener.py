1d7941f652181f6f7fe081db34aad070056828f9
Flemming Jahn <fjn@invisio.com>
2022-02-13 11:00:12 +0100

    Adding exclude list

--- nordnet_aktie_screener.py
+++ nordnet_aktie_screener.py
@@ -1,14 +1,41 @@
 import requests
 from optparse import OptionParser
+import json
+
 
 parser = OptionParser()
 parser.add_option("-a", "--pe_min", dest="pe_min", default=1,  help="Minimum P/E")
 parser.add_option("-b", "--pe_max", dest="pe_max", default=15, help="Maximum P/E")
 parser.add_option("-c", "--yield_min", dest="dividend_yield_min", default=5,    help="Minimum Direkte Rente")
 parser.add_option("-d", "--yield_max", dest="dividend_yield_max", default=100,  help="Maximum Direkte Rente")
-parser.add_option("-e", "--pawn_percentage_min", dest="instrument_pawn_percentage_min", default=70,    help="Minimum Belånings Grad")
-parser.add_option("-f", "--pawn_percentage_max", dest="instrument_pawn_percentage_max", default=100,  help="Maximum Belånings Grad")
+parser.add_option("-e", "--pawn_percentage_min", dest="instrument_pawn_percentage_min", default=75,    help="Minimum Belånings Grad")
+parser.add_option("-f", "--pawn_percentage_max", dest="instrument_pawn_percentage_max", default=101,  help="Maximum Belånings Grad")
+parser.add_option("-x", "--exclude", dest="exclude_stocks", default=None,  help="JSON file with List of stock to exclude ")
 (options, args) = parser.parse_args()
+
+
+
+
+def test_write():
+    exclude_list = []
+    my_stocks = ["ALMB", "FFF"]
+    my_stocks_json = {"my_stocks" : my_stocks}
+
+    exclude_stocks = ["TOP", "FFF"]
+    exclude_stocks_json = {"exclude_stocks" : exclude_stocks}
+
+    all_stocks_json = {"all_stocks": [my_stocks_json, exclude_stocks_json]}
+    json_object = json.dumps(all_stocks_json, indent=4)
+    with open("exclude_list.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+#test_write()
+options.exclude_stocks = "exclude_list.json"
+exclude_list = []
+if options.exclude_stocks is not None:
+    f = open(options.exclude_stocks)
+    exclude_list = json.load(f)
 
 
 cookies = {}
@@ -53,21 +80,31 @@ def get_ratios_value(info, key, key_min, key_max):
 
 def print_info(keyInfo, counter):
     for info in keyInfo['results']:
+        symbol = info['instrument_info']['symbol']
         name = get_name_print(info)
+
+        if name in exclude_list["all_stocks"][0]["my_stocks"]:
+            continue
+
+        if name in exclude_list["all_stocks"][1]["exclude_stocks"]:
+            continue
+
         pe   = get_ratios_value(info, 'pe', options.pe_min, options.pe_max)
         dividend_yield = get_ratios_value(info, 'dividend_yield', options.dividend_yield_min, options.dividend_yield_max)
         instrument_pawn_percentage = get_instrument_info_value(info, 'instrument_pawn_percentage', options.instrument_pawn_percentage_min, options.instrument_pawn_percentage_max)
 
+
         if None in (pe, dividend_yield, instrument_pawn_percentage):
             continue
         if "Fund" not in name:
-            print("%-4d:%-35s - %-10f - %-15f - %-5d" % (counter, name, pe, dividend_yield, instrument_pawn_percentage))
+            print("%-4d %-35s - %-10f - %-15f - %-5d" % (counter, name, pe, dividend_yield, instrument_pawn_percentage))
             counter += 1
     return counter
 
 
 def loop_over_stocks(counter, exchange_country, country):
     offset = 0
+    print("")
     print("*" * 100)
     print(f"* {country}" + " " * (100 - len(country) - 3) + "*")
     print("*" * 100)
@@ -91,8 +128,8 @@ def get_instrument_info_value(info, key, key_min, key_max):
 counter = loop_over_stocks(counter, 'DDK', "Danmark")
 counter = loop_over_stocks(counter, 'DSE', "Sverige")
 counter = loop_over_stocks(counter, 'DFI', "Finland")
-counter = loop_over_stocks(counter, 'DNO', "Norge")
-counter = loop_over_stocks(counter, 'DDE', "Tyskland")
+#counter = loop_over_stocks(counter, 'DNO', "Norge")
+#counter = loop_over_stocks(counter, 'DDE', "Tyskland")
 counter = loop_over_stocks(counter, 'DUS', "USA")
 counter = loop_over_stocks(counter, 'DCA', "Canada")
 
